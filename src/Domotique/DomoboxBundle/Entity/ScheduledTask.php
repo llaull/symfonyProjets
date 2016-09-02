0ad5b4565e8077c3d2ea2d44e74f0e069f665b6d
llaull <laul@llovem.eu>
2016-09-02 12:43:07 +0200

    ajout de la class repository

--- src/Domotique/DomoboxBundle/Entity/ScheduledTask.php
+++ src/Domotique/DomoboxBundle/Entity/ScheduledTask.php
@@ -10,6 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="domotique__task_scheduled")
  * @ORM\Entity
  * @ORM\HasLifecycleCallbacks
+ * @ORM\Entity(repositoryClass="Domotique\DomoboxBundle\Repository\ScheduledTaskRepository")
  */
 class ScheduledTask
 {
