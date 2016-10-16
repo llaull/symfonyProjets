d9754aef2819b03b9cf012c4479ba1dc23d1ec19
llaull <laul@llovem.eu>
2016-10-16 12:52:10 +0200

    Revert "c"
    This reverts commit a4d9d25282f302fa8100d368524e44bf477cc8fb.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,9 +32,6 @@ class AppKernel extends Kernel
             // WYSIWYG
             new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
             new FM\ElfinderBundle\FMElfinderBundle(),
-
-            //app my mouse
-            new MyMouseBundle\MouseBundle\MyMouseBundleMouseBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- src/MyMouseBundle/MouseBundle/MyMouseBundleMouseBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MyMouseBundle\MouseBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MyMouseBundleMouseBundle extends Bundle
-{
-}
